0cfec9975dcadfc22ee07b7b551393cc077d7ac5
Sérgio Estêvão <sergioestevao@gmail.com>
2024-07-26 18:42:40 +0100

    Improve code.

--- AssetExporter/AssetExporter.swift
+++ AssetExporter/AssetExporter.swift
@@ -5,28 +5,27 @@ struct AssetExporter {
 
     static func exportSession(forItem item: AVPlayerItem) async {
         let composition = AVMutableComposition()
-
-//        while item.status != .readyToPlay {
-//            try? await Task.sleep(for: .seconds(1))
-//        }
-
-
+        //        while item.status != .readyToPlay {
+        //            try? await Task.sleep(for: .seconds(1))
+        //        }
         guard let compositionAudioTrack = composition.addMutableTrack(withMediaType: .audio, preferredTrackID:  CMPersistentTrackID(kCMPersistentTrackID_Invalid)),
-            let sourceAudioTrack = try? await item.asset.loadTracks(withMediaType: .audio).first else {
+              let sourceAudioTrack = try? await item.asset.loadTracks(withMediaType: .audio).first else {
             print("Failed to create audio track")
             return
         }
         do {
-            try compositionAudioTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: item.asset.duration), of: sourceAudioTrack, at: CMTime.zero)
+            let duration = try await item.asset.load(.duration)
+            try compositionAudioTrack.insertTimeRange(CMTimeRangeMake(start: .zero, duration: duration), of: sourceAudioTrack, at: CMTime.zero)
         } catch {
             print("Failed to create audio track: \(error)")
             return
         }
-
+        
         guard let exporter = AVAssetExportSession(asset: composition, presetName: AVAssetExportPresetAppleM4A) else {
             print("Failed to create export session")
             return
         }
+
         let fileExtension = UTType(AVFileType.m4a.rawValue)?.preferredFilenameExtension ?? "m4a"
         let fileName = (item.asset as! AVURLAsset).url.lastPathComponent.replacingOccurrences(of: (item.asset as! AVURLAsset).url.pathExtension, with: fileExtension)
         let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).last!
@@ -42,7 +41,7 @@ struct AssetExporter {
 
         exporter.outputURL = outputURL
         exporter.outputFileType = AVFileType.m4a
-
+        exporter.shouldOptimizeForNetworkUse = true
         await exporter.export()
         print("Exporter did finish: \(outputURL)")
         if let error = exporter.error {
--- AssetExporter/ContentView.swift
+++ AssetExporter/ContentView.swift
@@ -6,7 +6,10 @@ var customLoaderDelegate: AssetExporterResourceLoaderDelegate?
 struct ContentView: View {
     // standard player used in all examples
     let player = AVPlayer()
-    @State var audioURL: String = "https://www.podtrac.com/pts/redirect.mp3/pdst.fm/e/chrt.fm/track/FACB75/traffic.megaphone.fm/NEXTDP6425035030.mp3?updated=1714419615"
+    @State var audioURL: String =
+        //"https://traffic.libsyn.com/secure/wellkeptwallet/Episode_134_-_Jillian.mp3?dest-id=142551"
+        //"https://traffic.libsyn.com/secure/wellkeptwallet/123_-_How_She_Became_Financially_Independent_By_Living_Overseas.mp3?dest-id=142551"
+        "https://sarahwallinhuff.com/wp-content/uploads/2024/06/Unit-2-b-AUDIO-2.mp3" //"https://www.podtrac.com/pts/redirect.mp3/pdst.fm/e/chrt.fm/track/FACB75/traffic.megaphone.fm/NEXTDP6425035030.mp3?updated=1714419615"
 
     var body: some View {
 
@@ -34,14 +37,14 @@ struct ContentView: View {
 
             // Example 3: Parallel Export session
             Button("Player with parallel export session") {
-                // must not use "https" or "http", the custom loader delegate will not be called!
+                player.pause()
                 let asset = AVURLAsset(url: URL(string: audioURL)!)
                 let item = AVPlayerItem(asset: asset)
+                player.replaceCurrentItem(with: item)
+                player.play()
                 Task {
                     await AssetExporter.exportSession(forItem: item)
                 }
-                player.replaceCurrentItem(with: item)
-                player.play()
             }.padding()
         }
 
